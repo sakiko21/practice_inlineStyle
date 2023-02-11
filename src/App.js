1453c4017f7e6e8aa0ab48284451a70d1b82be9e
sakiko21 <92835229+sakiko21@users.noreply.github.com>
2023-02-11 16:15:12 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { InlineStyle } from "./components/InlineStyle";
+import "./styles.css";
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
